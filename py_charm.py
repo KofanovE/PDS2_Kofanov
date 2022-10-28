38262bc0c2fe1ea5e86e0367803ac2dde87198a2
Evgen Kofanov <evgeniy.kofanov@gmail.com>
2022-10-28 22:03:09 +0300

    Change in py_charm.py from optimized branch

--- py_charm.py
+++ py_charm.py
@@ -1,8 +1,9 @@
 # New file created in pycharm
-
 print("First_change")
 print("Second_change")
 
 # commit 2
-
 print("Third_change")
+
+# changes in optimized branch
+print("Change in optimized branch")
