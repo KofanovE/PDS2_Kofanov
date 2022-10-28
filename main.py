c258e9089c09a125ea2490efe4eda733f4036fc2
Evgen Kofanov <evgeniy.kofanov@gmail.com>
2022-10-28 18:17:20 +0300

    commit_optimized_1 : create test_function for test input digit

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import random
 
 
+def test_function(x):
+    if not x.isdigit() or int(x) not in range(20):
+        x = random.randint(0, 20)
+        print(f"Digit was not right. I'm helping you: {x}"  )
+    return int(x)
+
 def main_function(x, sum_):
-    # Функція розрахунку суми послідовності цифр заданої кількості з використанням рекурсії
-    if x.isdigit() and 0 <= int(x) <= 20:
-        x = int(x)
-        while x > 0:
-            sum_ += x
-            x -= 1
-            main_function(str(x), sum_)
-        print(f"Sum = {sum_} ")
+    if x > 0:
+        main_function(x -= 1, sum_ += x)
+    else:
 
 
 
 
 
-
-
+        #x = int(x)
+        #if x > 0:
+        #    sum_ += x
+        #    x -= 1
+        #    main_function(str(x), sum_)
+        #else:
+        #    print(f"Sum = {sum_} ")
 
 
 sum_ = 0
-main_function(input("Enter your digit from 0 to 20: "), sum_)
+test_function(input("Enter your digit from 0 to 20: "))
