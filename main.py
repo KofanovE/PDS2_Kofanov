f7253a7fedd28ef8d376223eb5c4940be1b65f1b
Evgen Kofanov <evgeniy.kofanov@gmail.com>
2022-10-28 18:28:23 +0300

    commit_optimized_2 : finished optimized program

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 import random
 
-
 def test_function(x):
     if not x.isdigit() or int(x) not in range(20):
         x = random.randint(0, 20)
-        print(f"Digit was not right. I'm helping you: {x}"  )
+        print(f"Digit was not right. I'm helping you: {x}")
     return int(x)
 
 def main_function(x, sum_):
     if x > 0:
-        main_function(x -= 1, sum_ += x)
+        sum_ += x
+        x -= 1
+        main_function(x, sum_)
     else:
-
-
-
-
-
-        #x = int(x)
-        #if x > 0:
-        #    sum_ += x
-        #    x -= 1
-        #    main_function(str(x), sum_)
-        #else:
-        #    print(f"Sum = {sum_} ")
-
+        print(f"Sum = {sum_} ")
 
 sum_ = 0
-test_function(input("Enter your digit from 0 to 20: "))
+main_function(test_function(input("Enter your digit from 0 to 20: ")), sum_)
+
+
+
+
+
