300f95eedb19922806d3578f2e4d42f8be6f51ad
Evgen Kofanov <evgeniy.kofanov@gmail.com>
2022-10-28 17:24:44 +0300

    comit_1 : create new function in new file

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+sum = 0
+def main_function(x):
+    # Функція розрахунку суми послідовності цифр заданої кількості з використанням рекурсії
+    if x.isdigit() and 0 <= int(x) <= 20:
+
+
+
+
+
+
+
+
+
+
+main_function(input("Enter your digit from 0 to 20: "))
