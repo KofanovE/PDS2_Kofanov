7c3b2814b4a3806bdb2845302e6781745b3650fc
Evgen Kofanov <evgeniy.kofanov@gmail.com>
2022-10-28 17:30:12 +0300

    commit_2 : continue main_function

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ sum = 0
 def main_function(x):
     # Функція розрахунку суми послідовності цифр заданої кількості з використанням рекурсії
     if x.isdigit() and 0 <= int(x) <= 20:
+        while x >= 0:
+            sum += x
+            x -= 1
+            main_function(x)
 
 
 
