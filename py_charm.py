2831357ada512493b7073703260bf4b2981122c0
Evgen Kofanov <evgeniy.kofanov@gmail.com>
2022-10-28 22:57:17 +0300

    Add right coment

--- py_charm.py
+++ py_charm.py
@@ -1,3 +1,4 @@
+# Програма обчислює суму послідовності цифр від 0 до заданого з найменшою кількістю коду
 import random
 digit = (input("Enter your number from 0 to 10: "))
 right_digit = lambda  digit  : (digit if digit.isdigit() and 0 <= int(digit) <= 20 else random.randint(0, 20))
