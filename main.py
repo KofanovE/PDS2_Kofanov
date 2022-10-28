d582892c6851831fc5f7b5dcebe0a8b4380f402f
Evgen Kofanov <evgeniy.kofanov@gmail.com>
2022-10-28 17:40:22 +0300

    commit_3 create recursion function

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import random
-sum = 0
-def main_function(x):
+
+
+def main_function(x, sum_):
     # Функція розрахунку суми послідовності цифр заданої кількості з використанням рекурсії
     if x.isdigit() and 0 <= int(x) <= 20:
-        while x >= 0:
-            sum += x
+        x = int(x)
+        while x > 0:
+            sum_ += x
             x -= 1
-            main_function(x)
+            main_function(str(x), sum_)
+        print(f"Sum = {sum_} ")
 
 
 
@@ -16,5 +19,5 @@ def main_function(x):
 
 
 
-
-main_function(input("Enter your digit from 0 to 20: "))
+sum_ = 0
+main_function(input("Enter your digit from 0 to 20: "), sum_)
