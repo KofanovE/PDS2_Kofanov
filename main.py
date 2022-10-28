1bdeec638a959c49798685869ba9bc55925c3bdf
Evgen Kofanov <evgeniy.kofanov@gmail.com>
2022-10-28 17:57:20 +0300

    end of recursion function

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import random
 
-
 def main_function(x, sum_):
     # Функція розрахунку суми послідовності цифр заданої кількості з використанням рекурсії
     if x.isdigit() and 0 <= int(x) <= 20:
         x = int(x)
-        while x > 0:
+        if x > 0:
             sum_ += x
             x -= 1
             main_function(str(x), sum_)
-        print(f"Sum = {sum_} ")
-
-
-
-
-
-
-
+        else:
+            print(f"Sum = {sum_} ")
+    else:
+        x = random.randint(0, 20)
+        print(f"Your digit from 0 to 20 isn't right! I'm helping you: {x}")
+        main_function(str(x), sum_)
 
 
 sum_ = 0
