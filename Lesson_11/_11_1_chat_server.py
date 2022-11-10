8a661ad244e2506ed40b4e88d407500614632875
KofanovE <101435860+KofanovE@users.noreply.github.com>
2022-11-10 22:12:15 +0200

    Commit_3
    Modern files to  task _11_2_

--- Lesson_11/_11_1_chat_server.py
+++ Lesson_11/_11_1_chat_server.py
@@ -1,7 +1,13 @@
 import socket
+import pandas as pd
+import requests
 
 HOST = '127.0.0.1'
 PORT = 55000
+
+interval_var = '5min'
+symbol = 'ETH'
+url = f'https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol={symbol}&market=USD&interval={interval_var}&apikey=demo&datatype=csv'
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.bind((HOST, PORT))
@@ -12,8 +18,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         print('connected: ', addr)
         request = conn.recv(1024)
         print(str(request))
-        reply = input('Input your reply: ')
-        conn.send(bytes(reply, encoding='UTF-8'))
+        if request.isdigit():
+            if int(request) == 1:
+                df = pd.read_csv(url)
+                price = df['close'][0]
+                reply = f'Actual prise ETH is: {price}'
+                conn.send(bytes(reply, encoding='UTF-8'))
+            else:
+                reply = f'Unknown digit..'
+                conn.send(bytes(f'***If you want actual price ETH - press 1*** Bot:  {reply}', encoding='UTF-8'))
+        else :
+            reply = input('Input your reply: ')
+            conn.send(bytes(f'***If you want actual price ETH - press 1*** Bot:  {reply}', encoding='UTF-8'))
 
 
 
