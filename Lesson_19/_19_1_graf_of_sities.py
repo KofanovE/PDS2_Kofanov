6ada3da2058e61a18f08b978344ecd8961a99c68
Evgen Kofanov <evgeniy.kofanov@gmail.com>
2022-12-07 17:10:57 +0200

    Commit_19_0 Finished 1 task

---
+++ Lesson_19/_19_1_graf_of_sities.py
@@ -0,0 +1,21 @@
+# Використовуючі дані із файлу cities.csv, створіть список типу: cities = [["citi1", "citi2", km]...].
+# З отриманого списку створіть граф. Візуалізуйте отриманий граф.
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+edgelist = []
+with open("cities.csv") as file:
+    for line in file:
+        city_1, city_2, distance = line.strip().split(";")
+        edgelist.append([city_1, city_2, distance])
+
+graph = nx.Graph()
+for edge in edgelist:
+    graph.add_edge(edge[0], edge[1], distance = edge[2])
+
+pos = nx.spring_layout(graph)
+nx.draw_networkx(graph, pos)
+plt.title("Graph of Ukrainian cities")
+plt.show()
+
