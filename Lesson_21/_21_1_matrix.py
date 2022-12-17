874edeb24b0fcdc37508370261893b80301d7e22
Evgen Kofanov <evgeniy.kofanov@gmail.com>
2022-12-17 12:55:04 +0200

    Commit 21_3 Finished first task

--- Lesson_21/_21_1_matrix.py
+++ Lesson_21/_21_1_matrix.py
@@ -8,18 +8,21 @@ class MatrixClass:
     def __init__(self, obj):
         try:
             for i in range(len(obj)):
-                if type(obj) == list and type(obj[i]) == list:
+                if type(obj) == list and type(obj[i]) == list: #chek, that entered object is list of lists
+                    if obj == [[]]:
+                        self.matrix = [[]]
+                        return None
                     for j in obj[i]:
-                        if not (type(j) == int or type(j) == float):
+                        if not (type(j) == int or type(j) == float): #chek, that lists are including int or float digits
                             print("all elements of matrix must be digits")
                             self.matrix = [[]]
                             return None
-                    self.matrix = obj
-                else:
+                    self.matrix = obj #else, matrix is entered list of lists
+                else: # else not list of lists, created matrix is empty
                     print("obj isn't list of lists")
                     self.matrix = [[]]
                     return None
-        except TypeError:
+        except TypeError: # if not interate objects, matrix is empty
             print("obj isn't list of lists")
             self.matrix = [[]]
 
@@ -27,101 +30,174 @@ class MatrixClass:
         return self.matrix
 
 
+    def __matching(self, obj_2): # method om matching two matrix
+        lines = len(self.matrix) - len(obj_2.matrix) # matching lines
+        if lines > 0:
+            for i in range(lines):
+                obj_2.matrix.append([])
+        elif lines < 0:
+            for i in range(lines * (-1)):
+                self.matrix.append([])
+
+        for i in range(len(self.matrix)): # matching columns
+            columns = len(self.matrix[i]) - len(obj_2.matrix[i])
+            if columns > 0:
+                for j in range(columns):
+                    obj_2.matrix[i].append(0)
+            elif columns < 0:
+                for j in range(columns * (-1)):
+                    self.matrix[i].append(0)
+        num_members = 0
+        for i in range(len(self.matrix)):
+            if num_members < len(self.matrix[i]) or num_members < len(obj_2.matrix[i]):
+                if len(self.matrix[i]) >= len(obj_2.matrix[i]):
+                    num_members = len(self.matrix[i])
+                elif len(self.matrix[i]) < len(obj_2.matrix[i]):
+                    num_members = len(obj_2.matrix[i])
+        for i in range(len(self.matrix)):
+            if len(self.matrix[i]) < num_members:
+                self.matrix[i].append(0)
+            if len(obj_2.matrix[i]) < num_members:
+                obj_2.matrix[i].append(0)
 
 
     def __add_digit(self, obj_2):
+        if self.matrix == [[]]:
+            return None
         result = []
         for i in range(len(self.matrix)):
             result.append([])
             for j in range(len(self.matrix[i])):
                 result[i].append(self.matrix[i][j] + obj_2)
-        return MatrixClass(result)
+        self.matrix = result
+
+
 
     def __sub_digit(self, obj_2):
+        if self.matrix == [[]]:
+            return None
         result = []
         for i in range(len(self.matrix)):
             result.append([])
             for j in range(len(self.matrix[i])):
                 result[i].append(self.matrix[i][j] - obj_2)
-        return MatrixClass(result)
+        self.matrix = result
+
 
     def __mul_digit(self, obj_2):
+        if self.matrix == [[]]:
+            return None
         result = []
         for i in range(len(self.matrix)):
             result.append([])
             for j in range(len(self.matrix[i])):
                 result[i].append(self.matrix[i][j] * obj_2)
-        return MatrixClass(result)
-
-    def __del_digit(self, obj_2):
-        try:
-            result = []
-            for i in range(len(self.matrix)):
-                result.append([])
-                for j in range(len(self.matrix[i])):
-                    result[i].append(self.matrix[i][j] + obj_2)
-            return MatrixClass(result)
-        except ZeroDivisionError:
-            print("Error: divisor is zero!")
-            return None
+        self.matrix = result
 
 
     def __add_matrix(self, obj_2):
-        pass
+        self.__matching(obj_2)
+        result = []
+        for i in range(len(self.matrix)):
+            result.append([])
+            for j in range(len(self.matrix[i])):
+                result[i].append(self.matrix[i][j] + obj_2.matrix[i][j])
+        self.matrix = result
+
 
     def __sub_matrix(self, obj_2):
-        pass
+        self.__matching(obj_2)
+        result = []
+        for i in range(len(self.matrix)):
+            result.append([])
+            for j in range(len(self.matrix[i])):
+                result[i].append(self.matrix[i][j] - obj_2.matrix[i][j])
+        self.matrix = result
 
     def __mul_matrix(self, obj_2):
-        pass
+        if len(self.matrix[0]) != len(obj_2.matrix):
+            print("Imposible to multiply these matrixes!")
+            return None
+        result = []
+        for i in range(len(self.matrix)):
+            result.append([])
+            for i_2 in range(len(obj_2.matrix[0])):
+                sum = 0
+                for j in range(len(self.matrix[i])):
+                    sum += self.matrix[i][j] * obj_2.matrix[j][i_2]
+                result[i].append(sum)
+        self.matrix = result
 
-    def __del_matrix(self, obj_2):
-        pass
+
 
     def operations(self, obj_2, oper):
         if type(obj_2) == MatrixClass:
             if oper == "+":
-                result = self.__add_matrix(obj_2)
+                self.__add_matrix(obj_2)
             elif oper == "-":
-                result = self.__sub_matrix(obj_2)
+                self.__sub_matrix(obj_2)
             elif oper == "*":
-                result = self.__mul_matrix(obj_2)
-            elif oper == "/":
-                result = self.__del_matrix(obj_2)
+                self.__mul_matrix(obj_2)
             else:
                 print("unknown operation")
                 return None
         elif type(obj_2) == int or type(obj_2) == float:
             if oper == "+":
-                result = self.__add_digit(obj_2)
+                self.__add_digit(obj_2)
             elif oper == "-":
-                result = self.__sub_digit(obj_2)
+                self.__sub_digit(obj_2)
             elif oper == "*":
-                result = self.__mul_digit(obj_2)
-            elif oper == "/":
-                result = self.__del_digit(obj_2)
+                self.__mul_digit(obj_2)
             else:
                 print("unknown operation")
                 return None
         else:
             print("object from operation with matrix must be digit or matrix")
             return None
-        return result
+
 
     def tranponation(self):
-        pass
+        columns = 0
+        lines = 0
+        if len(self.matrix) > len(self.matrix[0]):
+            columns = len(self.matrix) - len(self.matrix[0])
+            for i in range(len(self.matrix)):
+                for j in range(columns):
+                    self.matrix[i].append(0)
+        elif  len(self.matrix) < len(self.matrix[0]):
+            lines = len(self.matrix[0]) - len(self.matrix)
+            for i in range(lines):
+                self.matrix.append([0 for j in range(len(self.matrix[0]))])
+
+        count = 0
+        for i in range(len(self.matrix)):
+            for j in range(count, len(self.matrix[i])):
+                if i != j:
+
+                        self.matrix[i][j], self.matrix[j][i] = self.matrix[j][i], self.matrix[i][j]
+            count += 1
+        if columns > 0:
+            for i in range(columns):
+                self.matrix.pop()
+        elif lines > 0:
+            for i in range(len(self.matrix)):
+                for j in range(lines):
+                    self.matrix[i].pop()
 
 
+try:
+    list_1 = [[1, 2, 3, 11, 12, 13], [4, 5, 6, 14, 15, 16]]
+    list_2 = [[1, 2], [4, 5], [7, 8]]
+    matrix_ = MatrixClass(list_1)
+    matrix_2 = MatrixClass(list_2)
+    print(matrix_.__str__())
+    print(matrix_2.__str__())
+    # matrix_.operations(matrix_2, "*")
+    #
+    # print(f"\n"*2)
 
-list_ = [[1, 2, 3], [4, 5, 6]]
-# list_ = ["a", 2, 3]
-matrix_ = MatrixClass(list_)
-matrix_2 = MatrixClass(list_)
-print(matrix_.__str__())
-print(type(matrix_.matrix))
-# matrix_.operations(matrix_2, "+")
-result = matrix_2.operations(2, '*')
-print(result.matrix)
+    matrix_2.tranponation()
+    print(matrix_2.__str__())
 
-
-
+except AttributeError:
+    None
